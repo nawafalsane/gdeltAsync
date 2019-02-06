b53ed7831c0be81a85934e8a2c32984b12c21979
nawafalsane <nawafalsane@gmail.com>
2019-02-06 23:26:59 +0300

    third commit of project

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from io import BytesIO,StringIO
 from zipfile import ZipFile
 from concurrent.futures import ThreadPoolExecutor
 import functools
+import asyncpg
 
 urls  = [
 "http://data.gdeltproject.org/gdeltv2/20150218230000.gkg.csv.zip",
@@ -38,7 +39,7 @@ urls  = [
 
 
 
-async def download_coroutine(session, url):
+async def download_coroutine(session, url, pool):
     async with session.get(url) as response:
         filename = os.path.basename(url)
         print("\033[35m" + f"BEGINING : Downloading {filename}" + "\033[0m" )
@@ -53,31 +54,37 @@ async def download_coroutine(session, url):
         loop = asyncio.get_event_loop()
         csvfile = await loop.run_in_executor(e, functools.partial(unzip, binaryfile, filename))
         result = await finder(csvfile)
-        return None
+        async with pool.acquire() as connection:
+            async with connection.transaction():
+                await connection.copy_records_to_table('gdelt', records=result)
+                # await connection.executemany('''
+                # insert into gdelt (GKGRECORDID,DATE,SourceCollectionIdentifier,SourceCommonName,DocumentIdentifier,Counts,V2Counts,Themes,V2Themes,Locations,V2Locations,Persons,V2Persons,Organizations,V2Organizations,V2Tone,Dates,GCAM,SharingImage,RelatedImages,SocialImageEmbeds,SocialVideoEmbeds,Quotations,AllNames,Amounts,TranslationInfo,Extras)
+                #  values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27);''', 
+                # result)
 
 def unzip(binarysteam, filename):
     print("\033[36m" + f"BEGINING: Unziping file {filename}" + "\033[0m")
     csvfile = filename[:-4]
     with ZipFile(binarysteam) as zip_ref:
          with zip_ref.open(csvfile) as myfile:
-            return myfile.read()
+            return myfile.readlines()
 
 async def finder(filename):
     lst = []
-    lstOfSubStrings = ["MEDICAL","SCIENCE","GENERAL_HEALTH","HEALTH_PANDEMIC","HEALTH_SEXTRANSDISEASE","HEALTH_VACCINATION","MEDICAL_SECURITY"]
-    async with aiofiles.open(filename) as csvfile:
-        pass
-        # async for line in csvfile:
-        #     subline = line.split("\t")[8]
-        #     if any(x in subline for x in lstOfSubStrings):
-        #         lst.append(line)
+    lstOfSubStrings = [b"MEDICAL",b"SCIENCE",b"GENERAL_HEALTH",b"HEALTH_PANDEMIC",b"HEALTH_SEXTRANSDISEASE",b"HEALTH_VACCINATION",b"MEDICAL_SECURITY"]
+    for line in filename:
+        subline = line.split(b"\t")[8]
+        if any(x in subline for x in lstOfSubStrings):
+            lst.append(tuple(line))
+        await asyncio.sleep(0)
                 
-    # return len(lst)
+    return lst
 
 async def main(loop):
-   
+    pool = await asyncpg.create_pool(database='nawafalsane', password="1001",user='nawafalsane')
+    # Configure service routes
     async with aiohttp.ClientSession(loop=loop) as session:
-        tasks = [download_coroutine(session, url) for url in urls[0:1]]
+        tasks = [download_coroutine(session, url,pool) for url in urls[0:1]]
         await asyncio.gather(*tasks)
 
 ## Create a producer and consumer patterne
